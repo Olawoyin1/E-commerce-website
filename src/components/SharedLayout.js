831e7d0baffbbc085207adc00b410c6c6d747422
Olawoyin <yustee2017@gmail.com>
2025-03-17 05:29:28 +0100

    ScrollToTop implemented

--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import ScrollToTop from "../ScrollToTo";
 
 const SharedLayout = ({ count, setSearchQuery,setOpenNav, modal, setModal }) => {
   return (
     <>
 
-    
+      <ScrollToTop />
       <Header count={count} setSearchQuery={setSearchQuery} />
       <Outlet />
       <Footer />
